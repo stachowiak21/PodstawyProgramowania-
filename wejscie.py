c98a4e40a7658c050387c4627e6ef46372099ddc
student1C <student1C@serwer.pl>
2025-10-07 14:33:56 +0200

    lekcja natemat wejście

---
+++ wejscie.py
@@ -0,0 +1,15 @@
+#1. wprowadzanie danych przez użytkownika do programu
+from zmienne import liczba_z_przecinkiem
+
+#1) utworzenie zmiennej
+#2) na ekranie pojawia sie komunikat z inputa
+#3) program sie zatrzymuje i czeka na podanie danych i zatwierdzenie tego ENTEREM
+#4) po wpisanie i zatwierdzebiu ENTEREM to co wpiszemy trafia jako dtring do zmiennej
+#5) i program wykonuje się dalej
+imie = input('Podaj swoje imię')
+
+liczba = int(input('Podaj liczbę'))
+print (type(liczba))
+
+liczba_z_przecinkiem = float(input('Podaj liczbę z przecinkiem'))
+print(type(liczba_z_przecinkiem))
