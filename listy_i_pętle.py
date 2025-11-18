4f168369f490932808bbf1392e1a4ea3283f6009
student1C <student1C@serwer.pl>
2025-11-18 14:35:21 +0100

    listy i pętei

--- listy_i_pętle.py
+++ listy_i_pętle.py
@@ -1,5 +1,4 @@
 #Jak nie programować wielokrotnie powtarzającej sie czynności
-from wejscie import liczba
 '''
 a = float(input('Podaj liczbe'))
 b = float(input('Podaj liczbe'))
@@ -20,5 +19,56 @@ for i in range (5)
 print(suma)'''
 
 #1. Listy
-lista =['qwerty', 56, [6, 7], 4.56, [[5, 8], 1]]
+'''lista =['qwerty', 56, [6, 7], 4.56, [[5, 8], 1]]
 print(lista[2][1])
+print(lista[4][0][1])'''
+
+#2. LIsty i pętle
+'''lista2 = ['kot', 'pies', 'owca', 'lama']'''
+
+#Pętla for:
+#-> wyciąga dane z listy (jeden po drugiej)
+#-> wykonuje siętyle razy ile elementów ma lista
+'''for z in lista2:
+    print(z)'''
+
+#Pętla, która wykona się 3 razy
+'''lista3 = [1410, 15, 7]
+
+for i in lista3:
+    print('OK')'''
+
+#Pętla, która wykonuje się 1000 razy
+'''lista4 = [0] * 1000
+
+for i in lista4:
+    print('Cześć')
+'''
+
+#Generatory i pętle
+'''przedzial = range (1,10)
+
+for i in przedzial:
+    print(i)'''
+
+#Pętla , która wykona sie 10 razy
+'''for i in range(10):
+    print(i)'''
+
+'''lista5 = [0]
+lista5.append(5)
+print(lista5)'''
+
+'''lista = [0]
+
+for i in lista:
+    print('Cześć')
+    lista.append(0)'''
+
+# Pętla while
+
+liczba = 5
+
+while liczba > 0:
+    print(liczba)
+    liczba = liczba - 1
