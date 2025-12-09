4728d35911b5cde7be6fd57e45066640c9ce027d
student1C <student1C@serwer.pl>
2025-12-09 14:32:36 +0100

    pętla while

---
+++ pętla_while.py
@@ -0,0 +1,36 @@
+#Pętla while - przykłądy
+
+'''liczba = 120
+licznik  = 0
+
+#W pętli while podajemy warunek TRWANIA pętli
+while liczba > 0: #tak długo jak liczba jest datnia(większa od zera), pętla sie wykonuje
+ liczba = liczba // 2
+    licznik = licznik + 1
+
+print(licznik)'''
+
+#Zadanie 1.
+'''x = input('Podaj liczbę lub q aby zakończyć: ')
+licznik = 0
+while x != 'q':
+    liczba = int(x)
+    if liczba < 2:
+        licznik = licznik + 1
+    x = input('Podaj liczbę lub q aby zakończyć: ')
+print(licznik)'''
+
+#Zadanie 2.
+
+popr_haslo = 'informatyka'
+haslo = input('Podaj hasło')
+proba = 1
+
+while haslo != popr_haslo and proba < 5:
+    print('Błędne hasło, podaj raz jeszcze!')
+    haslo = input('Podaj hasło')
+    proba = proba + 1
+if haslo == popr_haslo:
+    print('Witaj w systemie :)')
+else:
+    print('Nie ma hasła - nie ma dostępu')
