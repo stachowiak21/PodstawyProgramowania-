2e5db28c875c4774ffcb56e2956079495f832d80
stachowiak21 <stasa.stachowiak@gmail.com>
2025-12-10 19:31:09 +0100

    zadanie domowe pętle while zadanie numer 3 i 4
    Add a while loop that decrements n and sums even numbers.

--- pętla_while.py
+++ pętla_while.py
@@ -34,3 +34,25 @@ if haslo == popr_haslo:
     print('Witaj w systemie :)')
 else:
     print('Nie ma hasła - nie ma dostępu')
+
+#Zadanie 3.
+
+#pętla ta będzie wykonywała się cały 
+czas dopóki n jest większe lub
+równe 0 i jeżeli warunek że n jest 
+większe lub równe 0 sie spełni to
+od n program odejmie 1 więc nowe n 
+będzie o jeden mniejsze i jeśli nowe n przy 
+ dzieleniu przez 2 da reszte 1 to program nie 
+wypisze tej liczby w wyniku a wynik 
+to będą suma wszystkich liczb które są tym 
+zakresie i przy dzieleniu przez 2 nie dają reszty 2 
+
+#Zadanie 4.
+
+i = 10
+
+for x in range(i):
+    if i >= 1:
+        print(i)
+        i -= 1
