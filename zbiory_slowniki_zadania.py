4cad9a157172e85c5037ff1252e1ef2d32a3ccad
student1C <student1C@serwer.pl>
2026-02-16 11:23:41 +0100

    zbiory_słowniki zadania

--- zbiory_slowniki_zadania.py
+++ zbiory_slowniki_zadania.py
@@ -1,4 +1,4 @@
-zbior = set() #Pusty zbior
+'''zbior = set() #Pusty zbior
 
 lista2d = [
 [5, 2, 8, 5, 1],
@@ -19,4 +19,55 @@ zbior_cały = set()
 for x in lista2d:
     zbior2 = set(x)
     zbior_cały = zbior_cały.union(zbior2)
-print(zbior_cały)
+print(zbior_cały)'''
+
+slowa = [
+"LETTER",
+"BALLOON",
+"SUCCESS",
+"HAPPY",
+"COFFEE",
+"BOOKKEEPER",
+"ASSESS",
+"MISSISSIPPI",
+"ADDRESS",
+"TOOLBOX"
+]
+
+'''slowo = 'LETTER'
+slowo_zbior = set(slowo)
+print(len(slowo_zbior))'''
+
+'''for x in slowa:
+    x_zboir = set(x)
+    print(f'{x} {len(x_zboir)}')'''
+
+#Sposób 1
+'''max_x = ''
+max_l_r_l = 0
+
+for x in slowa:
+    x_zbior = set(x)
+    l_r_l = len(x_zbior)
+    if l_r_l > max_l_r_l:
+        max_l_r_l = l_r_l
+        max_x = x
+print(max_x)'''
+
+#Sposób 2
+zbior = set()
+
+'''max_slowo = max(slowa, key = lambda x: len(set(x)))
+print(max_slowo)'''
+
+#Zadanie 2.2
+for x in slowa:
+    for y in x:
+        zbior.add(y)
+
+for l in zbior:
+    lista = []
+    for s in sorted(slowa):
+        if l in s:
+            lista.append(s)
+    print(f'{l}: {lista}')
