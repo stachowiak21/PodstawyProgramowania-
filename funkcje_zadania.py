68788de3d3a39594e21a3bb985a020d911cd0b31
student1C <student1C@serwer.pl>
2026-03-02 11:22:42 +0100

    funkcje zadania

--- funkcje_zadania.py
+++ funkcje_zadania.py
@@ -18,7 +18,7 @@ print(wynik)'''
 
 #b)
 
-def iloczyn_s(u, v):
+'''def iloczyn_s(u, v):
     wynik = 0
     for i in range(len(u)):
         wynik += u[i] * v[i]
@@ -26,4 +26,29 @@ def iloczyn_s(u, v):
 u = [2, 7, 3]
 v = [-1, 0, 4]
 wynik = iloczyn_s(u, v)
-print(wynik)
+print(wynik)'''
+
+#Zadanie 2.1
+def czy_anagramy(s1, s2):
+    '''if sorted(s1) == sorted(s2):
+        return True
+    else:
+        return False'''
+
+    '''return sorted(s1) == sorted(2)'''#Krutsza opcja
+
+#print(czy_anagramy('nosek', 'keson'))
+
+'''s1 = 'nosek'
+s2 - 'keson'
+print(sorted(s1) == sorted(s2))'''
+
+def jaki_trojkat(a, b, c):
+    if a ** 2 + b ** 2 == c ** 2 == 2 * max([a, b, c]):
+        print('prostokątny')
+    elif a ** 2 + b ** 2 == c ** 2 > 2 * max([a, b, c]):
+        print('ostrokątny')
+    elif a ** 2 + b ** 2 == c ** 2 < 2 * max([a, b, c]):
+        print('rozwartokątny')
+
+jaki_trojkat(7, 10, 1)
